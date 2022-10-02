003ab28b71ef64f3bb7be7a67a37245a08607b59
parsTroy <troyparsons@hotmail.com>
2022-10-02 14:56:16 -0400

    updated footer and header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React from 'react';
+import { TiSocialFacebook, TiSocialTwitter, TiSocialYoutube } from 'react-icons/ti';
 
 export const Footer = () => {
   return (
-    <div className="bg-[#30475e] w-[100vw] flex justify-around items-center absolute bottom-0">
+    <div className='bg-[#30475e] w-[100vw] flex justify-around items-center absolute bottom-0 p-8'>
       <div>
         <h4>Contact</h4>
+        <form>
+          <label htmlFor=''></label>
+          <input type='text' value='Submit' />
+        </form>
       </div>
-      <div className="grid text-center">
-        <div className="m-2">
-          <h4>Facebook</h4> 
+      <div className='grid text-center'>
+        <div className='m-2 flex'>
+          <h4>
+            <TiSocialFacebook className='w-10 h-10' />
+          </h4> 
         </div>
-        <div className="m-2">
-          <h4>Twitter</h4> 
+        <div className='m-2 flex'>
+          <h4>
+            <TiSocialTwitter className='w-10 h-10' />
+          </h4>
         </div>
-          <div className="m-2">
-            <h4>Youtube</h4> 
+          <div className='m-2 flex'>
+            <h4>
+              <TiSocialYoutube className='w-10 h-10' />
+            </h4>
           </div>
         </div>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React from 'react';
+import { TiSocialFacebook, TiSocialTwitter, TiSocialYoutube } from 'react-icons/ti';
 
 const Header = () => {
   return (
-      <div className="bg-[#30475e] w-[100vw] h-24 flex justify-around items-center fixed">
-          <div className="w-44 h-14 border flex justify-center items-center p-2.5 font-mono font-extrabold">
+      <div className='bg-[#30475e] w-[100vw] h-24 flex justify-around items-center fixed'>
+          <div className='w-44 h-14 border flex justify-center items-center p-2.5 font-mono font-extrabold'>
             <h4>Grim Studios</h4>
           </div>
-          <div className="flex">
-            <div className="m-2">
+          <div className='flex'>
+            <div className='m-2'>
               <h4>
-                Facebook
+                <TiSocialFacebook className='w-10 h-10' />
               </h4> 
             </div>
-            <div className="m-2">
+            <div className='m-2'>
               <h4>
-                Twitter
+                <TiSocialTwitter className='w-10 h-10' />
               </h4> 
             </div>
-            <div className="m-2">
+            <div className='m-2'>
               <h4>
-                Youtube
+                <TiSocialYoutube className='w-10 h-10' />
               </h4> 
             </div>
           </div>
