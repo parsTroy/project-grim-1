5abc54e525f454183ccc5c7ce2d7a0661494ca23
parsTroy <troyparsons@hotmail.com>
2022-10-02 15:14:40 -0400

    footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,17 @@ export const Footer = () => {
   return (
     <div className='bg-[#30475e] w-[100vw] flex justify-around items-center absolute bottom-0 p-8'>
       <div>
-        <h4>Contact</h4>
-        <form>
-          <label htmlFor=''></label>
-          <input type='text' value='Submit' />
+        <h4 className='ml-2'>Contact</h4>
+        <form className='mt-2'>
+          <div>
+            <input type='text' placeholder='Name' className='w-54' />
+          </div>
+          <div>
+            <input type='email' placeholder='Email' className='w-54' />
+          </div>
+          <div>
+            <textarea placeholder='Message' className='w-[217px]' />
+          </div>
         </form>
       </div>
       <div className='grid text-center'>
