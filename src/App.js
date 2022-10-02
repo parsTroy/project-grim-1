671ec7affc12aed132b68f61b20e7051ee45f7b8
parsTroy <troyparsons@hotmail.com>
2022-10-02 17:04:34 -0400

    update formating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { Footer } from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
+import { Footer } from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
